41e62a94024e2e0d9bd89d2dfd69ad406aac3d1f
Clapboiz <phamconglap03@gmail.com>
2024-02-03 02:13:17 +0700

    print example

---
+++ Core PHP/Basic/printExample.php
@@ -0,0 +1,13 @@
+<?php
+echo "pi = ", 3.14, "\nx = ", 3;
+echo "\n"; // Add a new line for better formatting
+
+// Using print_r or var_dump to display the array,object
+print_r(["clap", "clap2k3"]);
+// OR
+// var_dump(["clap", "clap2k3"]); //print data's type
+
+//Do not print data's type
+var_export("hello");
+
+print "hello world";
