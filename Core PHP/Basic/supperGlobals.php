6cf4f9a5711da735df887c563b3914d9e3e335cb
Clapboiz <phamconglap03@gmail.com>
2024-02-05 17:16:24 +0700

    supper globals

--- Core PHP/Basic/supperGlobals.php
+++ Core PHP/Basic/supperGlobals.php
@@ -21,13 +21,23 @@ if (isset($_GET['age'])) {
 }
 
 // coalescing operator, the favorite way of almsot developers similar isset() function
-$name = $_POST['name'] ?? '';
-$age = $_POST['age'] ?? '';
+//Avoid hackers executing js commands and stealing data we use htmlspecialchars()
+// <script>alert('Code jsss')</script>
+$name = htmlspecialchars($_POST['name'] ?? '');
+$age = htmlspecialchars($_POST['age'] ?? '');
+
+//other way to prevent code js run
+$name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+$age = filter_var($_POST['age'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+// $name = addslashes(htmlspecialchars($_POST['name'])) ?? "";
+// $age = addslashes(htmlspecialchars($_POST['age'])) ?? "";
 
 echo $name;
 echo "<br>";
 echo $age;
 echo "<br>";
+
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +53,7 @@ echo "<br>";
 <!-- name send to browser must be _POST -->
 
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <div>
             <label for="name">Name:</label>
             <input type="text" id="name" name="name">
